b207ae5f323a5f267bd616d2e9b2b95c9bd498b5
IDO <susususu07@naver.com>
2024-01-16 05:00:51 +0900

    Add files via upload

---
+++ Datatype_basic.js
@@ -0,0 +1,16 @@
+/*  Data type 자료형 
+
+1. Number 숫자 => 사칙연산 가능
+
+1) 정수 Integer
+2) 소수 floating point
+
+2. String 문자열 => +로 문자열 연결 가능
+
+"", '' 둘다 사용가능하지만,
+양끝을 통일해줘야한다
+
+3. Boolean 불린 => 어떤 조건에 대한 결과 값으로 사용
+true & false 두 값만 가진다
+
+*/
